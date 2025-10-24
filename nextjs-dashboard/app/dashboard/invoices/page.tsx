96be185a9f36ea756c9999bb9061e60c94dec82d
Ragul <ragulkumarv333@gmail.com>
2025-10-24 19:40:43 +0530

    streaming grouping components

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <p>Dashboard Invoices Page</p>;
+export const dynamic = "force-dynamic";
+
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { Suspense } from "react";
+import { LatestInvoicesSkeleton } from "@/app/ui/skeletons";
+
+export default async function Page() {
+  return (
+    <main>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
 }
